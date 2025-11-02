8db0b480a72922233288dbac0274ab56d063ccdf
Raza Siddique <razamohd6395@gmail.com>
2025-11-02 18:22:39 +0530

    Day1 complete

--- Day-1_simple-number-problems/script.js
+++ Day-1_simple-number-problems/script.js
@@ -1,49 +1,8 @@
-// Brute force solution
+let num = 20;
 
-const num = 10;
-
-// sol:1
 for (let i = 1; i <= num; i++) {
-  // console.log(i);
-}
-
-// sol:2
-for (let i = 1; i <= num; i++) {
-  // console.log(num - i + 1);
-}
-
-// sol:3
-for (let i = 1; i <= num; i++) {
-  if (i % 2 === 0) {
-    // console.log(i);
+  let sqrt = Math.sqrt(i);
+  if (sqrt === Math.floor(sqrt) && i % 2 === 0) {
+    console.log(i);
   }
-}
-
-// sol:4
-let sum = 0;
-for (let i = 1; i <= num; i++) {
-  sum += i;
-}
-// console.log(sum);
-
-// sol:5
-let mul = 1;
-for (let i = 1; i <= num; i++) {
-  mul *= i;
-}
-// console.log(mul);
-
-// sol:6
-// let sum = 0;
-for (let i = 1; i <= num; i++) {
-  if (i % 2 === 0) {
-    sum += i;
-  }
-}
-// console.log(sum);
-
-// sol:7
-for (let i = 1; i <= num; i++) {
-  // console.log(Math.pow(i, 2));
-  // console.log(i ** 2);
 }
